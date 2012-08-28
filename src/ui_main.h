308932e74ceb630430089b606aedd74e65c979ca
mezger <mezger>
2012-08-28 15:39:45 +0000

    regenerated

--- src/ui_main.h
+++ src/ui_main.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'main.ui'
 **
-** Created: Thu Aug 23 16:35:44 2012
+** Created: Tue Aug 28 17:07:21 2012
 **      by: Qt User Interface Compiler version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
