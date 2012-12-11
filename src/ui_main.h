c73c42df64eda26d5ede9fe95371d98f57aa3fd8
mezger <mezger>
2012-12-11 17:00:14 +0000

    *** empty log message ***

--- src/ui_main.h
+++ src/ui_main.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'main.ui'
 **
-** Created: Wed Dec 5 18:01:39 2012
+** Created: Tue Dec 11 16:36:37 2012
 **      by: Qt User Interface Compiler version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
