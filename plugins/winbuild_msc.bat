b84436c72052b84decb2d32eaca0f13bd3010f5e
brands <brands>
2013-09-17 09:03:33 +0000

    build debugversion too

--- plugins/winbuild_msc.bat
+++ plugins/winbuild_msc.bat
@@ -3,14 +3,14 @@ echo Setting up environment for Qt usage...
 qmake plugins.pro CONFIG+=configA
 nmake.exe clean
 qmake plugins.pro CONFIG+=configA
-nmake
+nmake all
 
 qmake plugins.pro CONFIG+=configB
 nmake.exe clean
 qmake plugins.pro CONFIG+=configB
-nmake
+nmake all
 
 qmake plugins.pro CONFIG+=configC
 nmake.exe clean
 qmake plugins.pro CONFIG+=configC
-nmake
+nmake all
