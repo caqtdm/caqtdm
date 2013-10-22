2fd1d610ec6079ade57ff40be00856aba9721fea
mezger <mezger>
2013-10-22 10:29:12 +0000

    gpl text

--- src/limitsCartesianplotDialog.cpp
+++ src/limitsCartesianplotDialog.cpp
@@ -1,5 +1,29 @@
-#include <QtGui>
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
 
+#include <QtGui>
 #include "limitsCartesianplotDialog.h"
 
 limitsCartesianplotDialog::limitsCartesianplotDialog(caCartesianPlot *w, MutexKnobData *data, const QString &title, QWidget *parent) : QDialog(parent)
