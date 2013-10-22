6254c768273ab869d2a91b0dcd6d80a27b933412
mezger <mezger>
2013-10-22 10:25:51 +0000

    gpl text

--- src/caslider.cpp
+++ src/caslider.cpp
@@ -1,13 +1,27 @@
-//******************************************************************************
-// Copyright (c) 2012 Paul Scherrer Institut PSI), Villigen, Switzerland
-// Disclaimer: neither  PSI, nor any of their employees makes any warranty
-// or assumes any legal liability or responsibility for the use of this software
-//******************************************************************************
-//******************************************************************************
-//
-//     Author : Anton Chr. Mezger
-//
-//******************************************************************************
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
 
 #if defined(_MSC_VER)
 #include <windows.h>
@@ -18,6 +32,7 @@
 #include "caslider.h"
 #include <QtDebug>
 #include <QEvent>
+#include <QMouseEvent>
 #include <QApplication>
 #include <math.h>
 #include "alarmdefs.h"
@@ -198,6 +213,26 @@ void caSlider::setDirection(Direction dir)
     update();
 }
 
+void caSlider::mousePressEvent(QMouseEvent *e)
+{
+    if( e->button() == Qt::LeftButton) {
+
+        if(e->type() == QEvent::MouseButtonDblClick) {
+           e->ignore();
+           return;
+        } else {
+           e->accept();
+        }
+        QwtAbstractSlider::mousePressEvent(e);
+    }
+}
+
+void caSlider::mouseReleaseEvent( QMouseEvent *e )
+{
+    e->ignore();
+    QwtAbstractSlider::mouseReleaseEvent(e);
+}
+
 bool caSlider::eventFilter(QObject *obj, QEvent *event)
 {
         if (event->type() == QEvent::Enter) {
@@ -211,6 +246,10 @@ bool caSlider::eventFilter(QObject *obj, QEvent *event)
             QApplication::restoreOverrideCursor();
             setReadOnly(false);
             clearFocus();
+
+        //} else if(event->type() == QEvent::MouseButtonDblClick) {
+        //    printf("double click\n");
+        //    event->ignore();
         }
         return QObject::eventFilter(obj, event);
 }
--- src/qtcontrols_global.h
+++ src/qtcontrols_global.h
@@ -1,13 +1,27 @@
-//******************************************************************************
-// Copyright (c) 2012 Paul Scherrer Institut PSI), Villigen, Switzerland
-// Disclaimer: neither  PSI, nor any of their employees makes any warranty
-// or assumes any legal liability or responsibility for the use of this software
-//******************************************************************************
-//******************************************************************************
-//
-//     Author : Anton Chr. Mezger
-//
-//******************************************************************************
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
 
 #ifndef QTCONGLOBAL_H
 #define QTCONGLOBAL_H
