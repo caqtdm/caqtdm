9bcb492dbfac21d1908a3d8ef13a5b6bb1747062
mezger <mezger>
2013-10-22 10:34:04 +0000

    gpl text

--- src/fileopenwindow.cpp
+++ src/fileopenwindow.cpp
@@ -1,13 +1,27 @@
-//******************************************************************************
-// Copyright (c) 2012 Paul Scherrer Institut PSI), Villigen, Switzerland
-// Disclaimer: neither  PSI, nor any of their employees makes any warranty
-// or assumes any legal liability or responsibility for the use of this software
-//******************************************************************************
-//******************************************************************************
-//
-//     Author : Anton Chr. Mezger
-//
-//******************************************************************************
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
 
 #if defined(_MSC_VER)
    #include <windows.h>
@@ -437,8 +451,8 @@ void FileOpenWindow::Callback_OpenNewFile(const QString& inputFile, const QStrin
  */
 void FileOpenWindow::Callback_ActionAbout()
 {
-    QString message = QString("Qt-based Epics Display Manager Version %1 using %2 with data from %3 developed at Paul Scherrer Institut, by Anton Mezger\n");
-    message = message.arg(BUILDVERSION, BUILDARCH, SUPPORT);
+    QString message = QString("Qt-based Epics Display Manager Version %1 using Qt %2 and %3 with data from %4 developed at Paul Scherrer Institut, by Anton Mezger\n");
+    message = message.arg(BUILDVERSION, QT_VERSION_STR, BUILDARCH, SUPPORT);
     QTDMMessageBox *m = new QTDMMessageBox(QMessageBox::Information, "About", message, QMessageBox::Close, this, Qt::Dialog, true);
     m->show();
 }
