dc4b3d83001d2ef91f0f115be8d1eeb32f0d4cd4
mezger <anton.mezger@psi.ch>
2014-09-02 14:36:18 +0200

    new files for numerical display of waveforms

---
+++ caQtDM_QtControls/src/cawavetable.cpp
@@ -0,0 +1,160 @@
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
+
+#include <stdio.h>
+#include <QHeaderView>
+#include <QApplication>
+#include <QClipboard>
+#include "cawavetable.h"
+#include "alarmdefs.h"
+
+caWaveTable::caWaveTable(QWidget *parent) : QTableWidget(parent)
+
+{
+    setPrecisionMode(Channel);
+    setPrecision(0);
+
+    for(int i=0; i< MaxItems; i++) {
+        tableItem[i] = (QTableWidgetItem*)0;
+        setFormat(0);
+    }
+
+    thisItemFont = this->font();
+
+    setAlternatingRowColors(true);
+    setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+    setEditTriggers(QTableWidget::NoEditTriggers);
+    setSelectionMode(QAbstractItemView::NoSelection);
+    verticalHeader()->setDefaultSectionSize(40);
+    setOrientation(Horizontal);
+}
+
+void caWaveTable::setOrientation(Orientation orientation)
+{
+    thisOrientation = orientation;
+    if(orientation == Horizontal) {
+      setColumnCount(10);
+      setRowCount(1);
+
+    } else {
+        setColumnCount(1);
+        setRowCount(10);
+    }
+}
+
+void caWaveTable::setColumnSize(int newSize)
+{
+    thisColumnSize = newSize;
+    horizontalHeader()->setDefaultSectionSize(newSize);
+    horizontalHeader()->setVisible(false);
+    horizontalHeader()->setVisible(true);
+}
+
+void caWaveTable::setFormat(int prec)
+{
+    int precision;
+    if(thisPrecMode == User) {
+        precision = getPrecision();
+    } else {
+        precision = prec;
+    }
+    if(precision > 17) precision = 17;
+    if(precision >= 0) {
+        sprintf(thisFormat, "%s.%dlf", "%", precision);
+    } else {
+        sprintf(thisFormat, "%s.%dle", "%", -precision);
+    }
+}
+
+void caWaveTable::displayText(int index, QString const &text)
+{
+    if(index >= MaxItems) return;
+    if(keepText[index] == text) return;
+    if(index >= columnCount() && thisOrientation == Horizontal) return;
+    if(index >= rowCount() && thisOrientation == Vertical) return;
+    keepText[index] = text;
+
+    if(tableItem[index] != (QTableWidgetItem*) 0) {
+        tableItem[index]->setText(text);
+    } else {
+        tableItem[index] = new QTableWidgetItem(text);
+        if(thisOrientation == Horizontal) {
+          setItem(0, index, tableItem[index]);
+        } else {
+          setItem(index, 0, tableItem[index]);
+        }
+        tableItem[index]->setFont(thisItemFont);
+        setColumnWidth(index, thisColumnSize);
+    }
+}
+
+void caWaveTable::setValueFont(QFont font)
+{
+   thisItemFont = font;
+}
+
+QString caWaveTable::getPV() const
+    {
+       return thisPV;
+    }
+
+void caWaveTable::setPV(QString const &newPV)
+    {
+        thisPV = newPV;
+    }
+
+void caWaveTable::setData(double *array, int size)
+{
+     fillData(array, size);
+}
+
+void caWaveTable::setData(float *array, int size)
+{
+    fillData(array, size);
+}
+
+void caWaveTable::setData(int16_t *array, int size)
+{
+    fillData(array, size);
+}
+
+void caWaveTable::setData(int32_t *array, int size)
+{
+    fillData(array, size);
+}
+
+template <typename pureData>
+void caWaveTable::fillData(pureData *array, int size)
+{
+   string40 text;
+   int maxSize = MaxItems;
+   for(int i=0; i< qMin(size, maxSize); i++) {
+       sprintf(text, thisFormat, array[i]);
+       displayText(i, text);
+   }
+}
+
+
+
---
+++ caQtDM_QtControls/src/cawavetable.h
@@ -0,0 +1,109 @@
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
+
+#ifndef CAWAVETABLE_H
+#define CAWAVETABLE_H
+
+#include <QTableWidget>
+#include <QAction>
+#include <QFont>
+#include <qtcontrols_global.h>
+
+typedef char string40[40];
+
+class QTCON_EXPORT caWaveTable : public QTableWidget
+{
+
+    Q_OBJECT
+
+    Q_PROPERTY(QString channel READ getPV WRITE setPV)
+    Q_PROPERTY(int columnSize READ getColumnSize WRITE setColumnSize)
+    Q_PROPERTY(int precision READ getPrecision WRITE setPrecision)
+    Q_PROPERTY(SourceMode precisionMode READ getPrecisionMode WRITE setPrecisionMode)
+    Q_PROPERTY(Orientation orientation READ getOrientation WRITE setOrientation)
+    Q_ENUMS(SourceMode)
+    Q_ENUMS(Orientation)
+
+public:
+
+    caWaveTable(QWidget *parent);
+
+    enum SourceMode {Channel = 0, User};
+    SourceMode getPrecisionMode() const { return thisPrecMode; }
+    void setPrecisionMode(SourceMode precmode) {thisPrecMode = precmode;}
+
+    enum Orientation {Horizontal = 0, Vertical};
+    Orientation getOrientation() const { return thisOrientation; }
+    void setOrientation(Orientation orientation);
+    int getPrecision() const {return thisPrecision;}
+    void setPrecision(int prec) {thisPrecision = prec; setFormat(prec);}
+
+    int getColumnSize() const {return thisColumnSize;}
+    void setColumnSize(int newSize);
+
+    QString getPV() const;
+    void setPV(QString const &newPV);
+
+    void setFormat(int prec);
+    void displayText(int index, QString const &text);
+
+    void setValueFont(QFont font);
+
+    void setData(double *vector, int size);
+    void setData(float *vector, int size);
+    void setData(int16_t *vector, int size);
+    void setData(int32_t* vector, int size);
+
+signals:
+
+private slots:
+
+protected:
+
+private:
+
+   template <typename pureData>
+   void fillData(pureData *array, int size);
+
+    enum { MaxItems = 500 };
+    QString	thisPV;
+    int	thisColumnSize;
+
+    QString keepText[MaxItems];
+    string40 thisFormat;
+
+    int thisPrecision;
+    SourceMode thisPrecMode;
+
+    QFont thisItemFont;
+
+    QAction *copyAct;
+
+    QTableWidgetItem* tableItem[MaxItems];
+
+    Orientation thisOrientation;
+};
+
+#endif
