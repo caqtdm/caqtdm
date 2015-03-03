90fc2ab2e8593a2e14c29f326abf7f685b0398f9
marcel grunder <marcel.grunder@psi.ch>
2015-03-03 13:30:54 +0100

    comment for batch file and echo off

--- caQtDM_Tests/Display/TST-CAQTDM-MAIN.bat
+++ caQtDM_Tests/Display/TST-CAQTDM-MAIN.bat
@@ -1,3 +1,9 @@
+@echo off
+REM ****************************************
+REM 
+REM Startscript for test display
+REM 
+REM ****************************************
 set AUTO_ADDR_LIST=NO
 set EPICS_CA_ADDR_LIST=129.129.130.255
 
