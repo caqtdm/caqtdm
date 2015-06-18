66f45d59f58a5021ad81b3b2dec30d4d0aadef7e
mezger <anton.mezger@psi.ch>
2015-06-18 12:26:25 +0200

    message queue added

---
+++ caQtDM_QtControls/src/messageQueue.h
@@ -0,0 +1,46 @@
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
+
+#ifndef MESSAGEQUEUE_H
+#define MESSAGEQUEUE_H
+
+#include <QString>
+#include <QQueue>
+#include <qtcontrols_global.h>
+
+class QTCON_EXPORT messageQueue
+{
+public:
+   messageQueue();
+   void enqueue(const QString &message);
+   QString dequeue();
+   bool isEmpty();
+
+private:
+   QQueue<QString> queue;
+   
+};
+   
+#endif
