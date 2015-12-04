415cb7997778259404ef498e9b0b65e8be3fb07f
Helge Brands <helge.brands@psi.ch>
2015-12-04 10:06:59 +0100

    fix of deprecation of include file

--- caQtDM_QtControls/plugins/qtcontrols_controllers_plugin.h
+++ caQtDM_QtControls/plugins/qtcontrols_controllers_plugin.h
@@ -27,7 +27,12 @@
 #define QTCONTROLS_CONTROLLERS_PLUGIN_H
 
 #include <qglobal.h>
-#include <QDesignerCustomWidgetInterface>
+#if QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+    #include <QtUiPlugin/QDesignerCustomWidgetInterface>
+#else
+    #include <QDesignerCustomWidgetInterface>
+#endif
+
 
 class CustomWidgetInterface_Controllers: public QObject, public QDesignerCustomWidgetInterface
 {
--- caQtDM_QtControls/plugins/qtcontrols_graphics_plugin.h
+++ caQtDM_QtControls/plugins/qtcontrols_graphics_plugin.h
@@ -27,7 +27,11 @@
 #define QTCONTROLS_GRAPHICS_PLUGIN_H
 
 #include <qglobal.h>
-#include <QDesignerCustomWidgetInterface>
+#if QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+    #include <QtUiPlugin/QDesignerCustomWidgetInterface>
+#else
+    #include <QDesignerCustomWidgetInterface>
+#endif
 
 class CustomWidgetInterface_Graphics: public QObject, public QDesignerCustomWidgetInterface
 {
