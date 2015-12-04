4c39b0c2b083b7b71d2894305c625a7df1c81862
AntonMezger <anton.mezger@psi.ch>
2015-12-04 11:18:23 +0100

    modified qt_version_check while in Qt4.8 not supported by the moc compiler

--- caQtDM_QtControls/plugins/qtcontrols_graphics_plugin.h
+++ caQtDM_QtControls/plugins/qtcontrols_graphics_plugin.h
@@ -27,7 +27,7 @@
 #define QTCONTROLS_GRAPHICS_PLUGIN_H
 
 #include <qglobal.h>
-#if QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+#if QT_VERSION > 0x050000
     #include <QtUiPlugin/QDesignerCustomWidgetInterface>
 #else
     #include <QDesignerCustomWidgetInterface>
