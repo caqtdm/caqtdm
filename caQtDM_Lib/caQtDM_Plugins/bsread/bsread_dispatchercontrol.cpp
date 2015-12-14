b0e9801fad9ef2848fb428c9f79a555d6aacbabe
Helge Brands <helge.brands@psi.ch>
2015-12-14 11:04:28 +0100

    start dispatcher object

---
+++ caQtDM_Lib/caQtDM_Plugins/bsread/bsread_dispatchercontrol.cpp
@@ -0,0 +1,7 @@
+#include "bsread_dispatchercontrol.h"
+
+bsread_dispatchercontrol::bsread_dispatchercontrol(QString *dispatcher) : QObject(parent)
+{
+
+}
+
---
+++ caQtDM_Lib/caQtDM_Plugins/bsread/bsread_dispatchercontrol.h
@@ -0,0 +1,16 @@
+#ifndef BSREAD_DISPATCHERCONTROL_H
+#define BSREAD_DISPATCHERCONTROL_H
+
+
+class bsread_dispatchercontrol : public QObject
+{
+
+public:
+    explicit bsread_dispatchercontrol(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // BSREAD_DISPATCHERCONTROL_H
