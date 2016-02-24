9b9b0317e1fd70d07672acc85e57cca36e460405
AntonMezger <anton.mezger@psi.ch>
2016-02-24 10:20:46 +0100

    caLineDemo missing files

---
+++ caQtDM_QtControls/src/calinedemo.cpp
@@ -0,0 +1,566 @@
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
+
+#include "calinedemo.h"
+#include "alarmdefs.h"
+
+#include <QPainter>
+#include <QDebug>
+
+caLineDemo::caLineDemo(QWidget *parent) : QWidget(parent), FontScalingWidget(this), caWidgetInterface()
+{
+    // we want this font, while nice and monospace
+    QFont font("Lucida Sans Typewriter");
+    // if this font does not exist then try a next one
+    QFontInfo info(font);
+    //font.setStyleStrategy(QFont::NoAntialias);
+    QString family = info.family();
+    //printf("got font %s\n", qasc(family));
+    if(!family.contains("Lucida Sans Typewriter")) {
+        QFont  newfont("Monospace");   // not very nice, while a a dot inside the zero to distinguish from o
+        newfont.setStyleHint(QFont::TypeWriter);
+        setFont(newfont);
+    } else {
+        setFont(font);
+    }
+
+    m_Text = "text";
+    m_IsShown = false;
+    m_BackColor = QColor(255,255,255,0);
+    m_ForeColor = Qt::black;
+    m_BackColorOld = Qt::black;
+    m_ForeColorOld = Qt::gray;
+    m_FrameColor=Qt::black;
+    m_FrameLineWidth = 0;
+
+    m_ColorMode=Default;
+    m_AlarmHandling = onForeground;
+    m_Alignment = Center;
+    m_FormatType = decimal;
+    m_UnitMode = false;
+    m_PrecMode = Channel;
+    m_LimitsMode = Channel;
+    m_Precision = 0;
+    m_FormatType = decimal;
+
+    m_Maximum = 1.0;
+    m_Minimum = 0.0;
+
+    m_Format[0] = '\0';
+    m_FormatC[0] = '\0';
+    setFormat(0);
+
+    setFontScaleModeL(WidthAndHeight);
+
+    setFocusPolicy(Qt::NoFocus);
+
+    m_AlarmState = 0;
+
+    setText(" ");
+}
+
+void caLineDemo::setFrame(bool frame) {
+    m_FramePresent = frame;
+    if(!m_FramePresent) setLinewidth(0);
+}
+
+void caLineDemo::setFrameColor(QColor c) {
+    m_FrameColor = c;
+    setColors(m_BackColor, m_ForeColor, m_FrameColor);
+}
+
+void caLineDemo::setLinewidth(int width)
+{
+    if(width < 0) m_FrameLineWidth = 0;
+    else m_FrameLineWidth = width;
+    update();
+}
+
+void caLineDemo::setAlignment(const Alignment &alignment)
+{
+    m_Alignment = alignment;
+    update();
+}
+
+void caLineDemo::setBackground(QColor c)
+{
+    m_BackColor = c;
+    setColors(m_BackColor, m_ForeColor, m_FrameColor);
+}
+
+void caLineDemo::setForeground(QColor c)
+{
+    m_ForeColor = c;
+    setColors(m_BackColor, m_ForeColor, m_FrameColor);
+}
+
+void caLineDemo::setColors(QColor bg, QColor fg, QColor frame)
+{
+    if(!m_BackColorDefault.isValid() || !m_ForeColorDefault.isValid()) return;
+
+    if((bg != m_BackColorOld) || (fg != m_ForeColorOld) || (m_ColorMode != m_ColorModeOld) || (frame != m_FrameColorOld)) {
+
+        m_FrameColorTop = frame.darker();
+        m_FrameColorBottom = frame.lighter();
+
+        // default = (colors from stylesheet)
+        if(m_ColorMode == Default) {
+            m_BackColor = m_BackColorDefault;
+            m_ForeColor = m_ForeColorDefault;
+
+        // alarm default = alarm colors on foreground or background (colors from alarms and stylesheet)
+        // when major alarm and background handling take the background from stylesheet (normally would be white)
+        } else if(m_ColorMode == Alarm_Default) {
+
+            if(m_AlarmState == MAJOR_ALARM && m_AlarmHandling == onBackground) {
+                m_BackColor = bg;
+                m_ForeColor = m_BackColorDefault;
+            } else {
+                if(m_AlarmHandling == onForeground) {
+                    m_BackColor = m_BackColorDefault;
+                    m_ForeColor = fg;
+                } else {
+                    m_BackColor = bg;
+                    m_ForeColor = m_ForeColorDefault;
+                }
+            }
+
+        // alarm alarm colors on foreground or background (colors from color properties)
+        } else if(m_ColorMode == Alarm_Static) {
+            m_BackColor = bg;
+            m_ForeColor = fg;
+
+        // static (colors from color properties)
+        } else {
+            m_BackColor = bg;
+            m_ForeColor = fg;
+        }
+        update();
+    }
+    m_BackColorOld = bg;
+    m_ForeColorOld = fg;
+    m_ColorModeOld = m_ColorMode;
+}
+
+void caLineDemo::forceForeAndBackground(QColor fg, QColor bg)
+{
+    colMode aux = m_ColorMode;
+    m_ColorMode = Alarm_Static;
+    setColors(fg, bg, m_FrameColor);
+    m_ColorMode = aux;
+}
+
+void caLineDemo::setForeAndBackground(QColor foreground, QColor background)
+{
+    m_ForeColor = foreground;
+    m_BackColor = background;
+    setColors(m_BackColor, m_ForeColor, m_FrameColor);
+}
+
+void caLineDemo::setAlarmColors(short status, double value, QColor bgAtInit, QColor fgAtInit)
+{
+    QColor c;
+    m_AlarmState = 0;
+
+    if(status != NOTCONNECTED) {
+        if(m_LimitsMode == Channel) {
+            m_AlarmState = status;
+        } else if(m_LimitsMode == User) {
+            if(value > getMaxValue() || value < getMinValue()) {
+                m_AlarmState = MAJOR_ALARM;
+            } else {
+                m_AlarmState = NO_ALARM;
+            }
+        } else {
+            //return;
+        }
+    } else {
+        m_AlarmState = status;
+    }
+
+    switch (m_AlarmState) {
+
+    case NO_ALARM:
+        //qDebug() << "no alarm" << kPtr->pv;
+        if(m_ColorMode == Alarm_Static || m_ColorMode == Alarm_Default) {
+            c = AL_GREEN;
+            if(m_AlarmHandling == onForeground) setForeAndBackground(c, bgAtInit);
+            else setForeAndBackground(fgAtInit, c);
+        } else {
+            setForeAndBackground(fgAtInit, bgAtInit);
+        }
+        break;
+
+    case MINOR_ALARM:
+        //qDebug() << "minor alarm";
+        if(m_ColorMode == Alarm_Static || m_ColorMode == Alarm_Default) {
+            c = AL_YELLOW;
+            if(m_AlarmHandling == onForeground) setForeAndBackground(c, bgAtInit);
+            else setForeAndBackground(fgAtInit, c);
+        } else {
+            setForeAndBackground(fgAtInit, bgAtInit);
+        }
+        break;
+
+    case MAJOR_ALARM:
+        //qDebug() << "serious alarm" << kPtr->pv;
+        if(m_ColorMode == Alarm_Static || m_ColorMode == Alarm_Default) {
+            c = AL_RED;
+            if(m_AlarmHandling == onForeground) setForeAndBackground(c, bgAtInit);
+            else setForeAndBackground(fgAtInit, c);
+        } else {
+            setForeAndBackground(fgAtInit, bgAtInit);
+        }
+        break;
+
+    case INVALID_ALARM:
+        //qDebug() << "invalid alarm";
+        if(m_ColorMode == Alarm_Static) {
+            c =AL_WHITE;
+            if(m_AlarmHandling == onForeground) setForeAndBackground(c, bgAtInit);
+            else setForeAndBackground(fgAtInit, c);
+        } else {
+            setForeAndBackground(fgAtInit, bgAtInit);
+        }
+        break;
+
+    case NOTCONNECTED:
+        //qDebug() << "no connection";
+        forceForeAndBackground(AL_WHITE, AL_WHITE);
+        break;
+
+    default:
+        //qDebug() << "Alarm default" << status;
+        if(m_ColorMode == Alarm_Static) {
+            c = AL_DEFAULT;
+            if(m_AlarmHandling == onForeground) setForeAndBackground(c, bgAtInit);
+            else setForeAndBackground(fgAtInit, c);
+        } else {
+            setForeAndBackground(fgAtInit, bgAtInit);
+        }
+
+        break;
+    }
+
+    m_bgAtInitLast = bgAtInit;
+    m_fgAtInitLast = fgAtInit;
+}
+
+void caLineDemo::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    QBrush brush = QBrush(m_BackColor);
+    painter.setPen(m_ForeColor);
+    painter.setBackground(brush);
+    painter.setBackgroundMode(Qt::OpaqueMode);
+    painter.fillRect(0,0, width(), height(), brush);
+    QRect newRect(rect().top() + m_FrameLineWidth + 1, rect().left() + m_FrameLineWidth+1, rect().width() - 2 * m_FrameLineWidth - 2, rect().height() - 2 * m_FrameLineWidth - 2);
+    switch (m_Alignment) {
+    case Left:
+        painter.drawText(newRect, Qt::AlignLeft | Qt::AlignVCenter, m_Text);
+        break;
+    case Right:
+        painter.drawText(newRect, Qt::AlignRight | Qt::AlignVCenter, m_Text);
+        break;
+    case Center:
+    default:
+        painter.drawText(newRect, Qt::AlignCenter | Qt::AlignVCenter, m_Text);
+        break;
+    }
+
+    if(m_FramePresent) {
+        painter.setPen(QPen(m_FrameColorBottom, m_FrameLineWidth));
+        painter.drawLine(QPoint(0, height() - m_FrameLineWidth/2), QPoint(width(), height() - m_FrameLineWidth/2));
+        painter.drawLine(QPoint(width() -  m_FrameLineWidth/2, m_FrameLineWidth/2), QPoint(width() -  m_FrameLineWidth/2, height() - m_FrameLineWidth/2));
+        painter.setPen(QPen(m_FrameColorTop, m_FrameLineWidth));
+        painter.drawLine(QPoint(0, m_FrameLineWidth/2), QPoint(width(), m_FrameLineWidth/2));
+        painter.drawLine(QPoint(m_FrameLineWidth/2, m_FrameLineWidth/2), QPoint(m_FrameLineWidth/2, height() - m_FrameLineWidth/2));
+    }
+}
+
+bool caLineDemo::event(QEvent *e)
+{
+    if(e->type() == QEvent::Resize || e->type() == QEvent::Show) {
+        FontScalingWidget::rescaleFont(m_Text, calculateTextSpace());
+
+        // we try to get the default color for the background set through the external stylesheets
+        if(!m_IsShown) {
+            QString c = palette().color(QPalette::Base).name();
+            m_BackColorDefault = QColor(c);
+            //printf("default back color %s %s\n", qasc(c), qasc(this->objectName()));
+            c = palette().color(QPalette::Text).name();
+            m_ForeColorDefault = QColor(c);
+            //printf("default fore color %s %s\n", qasc(c), qasc(this->objectName()));
+            if(!m_BackColorDefault.isValid()) m_BackColorDefault = QColor(255, 248, 220, 255);
+            if(!m_ForeColorDefault.isValid()) m_ForeColorDefault = Qt::black;
+
+            setColors(m_BackColor, m_ForeColor, m_FrameColor);
+            m_IsShown = true;
+        }
+    }
+
+    return QWidget::event(e);
+}
+
+QSize caLineDemo::calculateTextSpace()
+{
+    d_savedTextSpace = contentsRect().size();
+    d_savedTextSpace.setWidth(d_savedTextSpace.width() - 2*m_FrameLineWidth);
+    d_savedTextSpace.setHeight(d_savedTextSpace.height() - 2*m_FrameLineWidth);
+    return d_savedTextSpace;
+}
+
+QSize caLineDemo::sizeHint() const
+{
+    if(!fontScaleEnabled())
+    {
+        return QWidget::sizeHint();
+    }
+    QFont f = font();
+    f.setPointSize(4);
+    QFontMetrics fm(f);
+    int w = fm.width(m_Text);
+    int h = fm.height();
+    /* add some pixels... */
+    w += 4;
+    h += 4;
+    QSize size(w, h);
+    return size;
+}
+
+QSize caLineDemo::minimumSizeHint() const
+{
+    QSize size;
+    if(!fontScaleEnabled())
+        size = QWidget::minimumSizeHint();
+    else
+        size = sizeHint();
+
+    return size;
+}
+
+void caLineDemo::setText(const QString &txt)
+{
+    if(m_Text == txt) return;
+    if(m_Text.size() != txt.size()) {
+        FontScalingWidget::rescaleFont(txt, d_savedTextSpace);
+    }
+    m_Text = txt;
+    //update();
+    repaint();
+}
+
+void caLineDemo::setFormat(int prec)
+{
+    int precision = prec;
+    if(precision > 17) precision = 17;
+    if(m_PrecMode == User) {
+        precision = getPrecision();
+    }
+    switch (m_FormatType) {
+    case string:
+    case decimal:
+        if(precision >= 0) {
+            sprintf(m_Format, "%s.%dlf", "%", precision);
+        } else {
+            sprintf(m_Format, "%s.%dle", "%", -precision);
+        }
+        break;
+    case compact:
+        sprintf(m_Format, "%s.%dle", "%", qAbs(precision));
+        sprintf(m_FormatC, "%s.%dlf", "%", qAbs(precision));
+        break;
+    case exponential:
+    case engr_notation:
+        sprintf(m_Format, "%s.%dle", "%", qAbs(precision));
+        break;
+    case truncated:
+    case enumeric:
+        strcpy(m_Format, "%d");
+        break;
+    case hexadecimal:
+        strcpy(m_Format, "0x%x");
+        break;
+    case octal:
+        strcpy(m_Format, "O%o");
+        break;
+    case sexagesimal:
+    case sexagesimal_hms:
+    case sexagesimal_dms:
+        break;
+    }
+}
+
+void caLineDemo::setValue(double value, const QString& units)
+{
+    char asc[1024];
+    if(m_FormatType == compact) {
+        if ((value < 1.e4 && value > 1.e-4) || (value > -1.e4 && value < -1.e-4) || value == 0.0) {
+            sprintf(asc, m_FormatC, value);
+        } else {
+            sprintf(asc, m_Format, value);
+        }
+    } else if(m_FormatType == hexadecimal || m_FormatType == octal)  {
+        sprintf(asc, m_Format, (int) value);
+    } else if(m_FormatType == truncated) {
+        sprintf(asc, m_Format, (int) value);
+    } else {
+        sprintf(asc, m_Format, value);
+    }
+    if(m_UnitMode) {
+        strcat(asc, " ");
+        strcat(asc, qasc(units));
+    }
+    setText(asc);
+}
+
+
+// caWidgetInterface implementation
+void caLineDemo::caDataUpdate(const QString& units, const QString& String, const knobData& data)
+{
+    QColor bg = property("BColor").value<QColor>();
+    QColor fg = property("FColor").value<QColor>();
+
+    if(data.edata.connected) {
+        // enum string
+        if(data.edata.fieldtype == caENUM || data.edata.fieldtype == caSTRING || data.edata.fieldtype == caCHAR) {
+            QStringList list;
+            if(m_ColorMode == Static || m_ColorMode == Default) { // done at initialisation
+                if(!property("Connect").value<bool>()) {                    // but was disconnected before
+                    setAlarmColors(data.edata.severity, (double) data.edata.ivalue, bg, fg);
+                    setProperty("Connect", true);
+                }
+            } else {
+                setAlarmColors(data.edata.severity, (double) data.edata.ivalue, bg, fg);
+            }
+            list = String.split((QChar)27);
+
+            if((data.edata.fieldtype == caENUM)  && (list.count() == 0)) {
+                QString str= QString::number((int) data.edata.ivalue);
+                setText(str);                              // no string, but number
+            } else if((data.edata.fieldtype == caENUM)  && ((int) data.edata.ivalue < list.count() ) && (list.count() > 0)) {
+                if(list.at((int) data.edata.ivalue).trimmed().size() == 0)  {  // string seems to empty, give value
+                    QString str= QString::number((int) data.edata.ivalue);
+                    setText(str);
+                } else {                                                       // we have a string, we want as string
+                    if(m_FormatType != enumeric) {
+                        setText(list.at((int) data.edata.ivalue));
+                    } else {
+                        QString str= QString::number((int) data.edata.ivalue); // we have  astring, we want as number
+                        setText(str);
+                    }
+                }
+            } else if((data.edata.fieldtype == caENUM)  && ((int) data.edata.ivalue >= list.count()) && (list.count() > 0)) {
+                QString str= QString::number((int) data.edata.ivalue);
+                setText(str);
+            } else if (data.edata.fieldtype == caENUM) {
+                setText("???");
+
+                // just one char (display as character when string format is specified, otherwise as number in specified format
+            } else if((data.edata.fieldtype == caCHAR) && (data.edata.nelm == 1)) {
+                if(m_FormatType == string) {
+                    QString str = QString(QChar((int) data.edata.ivalue));
+                    setText(str);
+                } else {
+                    setValue(data.edata.ivalue, "");
+                }
+
+                // one or more strings, or a char array
+            } else {
+                if(data.edata.nelm == 1) {
+                    setText(String);
+                } else if(list.count() > 0) {
+                    setText(list.at(0));
+                }
+            }
+
+            // double
+        } else {
+
+            if(m_ColorMode == Static || m_ColorMode == Default) { // done at initialisation
+                if(property("Connect").value<bool>()) {                      // but was disconnected before
+                    setAlarmColors(data.edata.severity, data.edata.rvalue, bg, fg);
+                    setProperty("Connect", true);
+                }
+            } else {
+                setAlarmColors(data.edata.severity, data.edata.rvalue, bg, fg);
+            }
+
+            if((m_PrecMode != User) && (data.edata.initialize)) {
+                setFormat(data.edata.precision);
+            }
+            setValue(data.edata.rvalue, units);
+        }
+
+    } else {
+        setText("");
+        setAlarmColors(NOTCONNECTED, 0.0, bg, fg);
+        setProperty("Connect", false);
+    }
+
+}
+
+void caLineDemo::caActivate(CaQtDM_Lib_Interface* interface, QMap<QString, QString> map, knobData* kData, int* specData, QWidget* parent)
+{
+    if(getPV().size() > 0) {
+        QString pv;
+        interface->addMonitor(parent, kData, getPV(), (QWidget *) this, specData, map, &pv);
+        setPV(pv);
+    }
+    setFormat(1);
+    setProperty("Taken", true);
+    caDataInterface = interface;
+}
+
+void caLineDemo::createContextMenu(QMenu& menu){
+    // construct info for the pv we are pointing at
+    menu.addAction("Get Info");
+}
+
+void caLineDemo::getWidgetInfo(QString* pv, int& nbPV, int& limitsDefault, int& precMode, int& limitsMode,
+                               int& Precision, char* colMode, double& limitsMax, double& limitsMin){
+    Q_UNUSED(limitsDefault);
+
+    pv[0] = getPV().trimmed();
+    if(getPrecisionMode() == User) {
+        precMode = true;
+        Precision = getPrecision();
+    }
+    if(getLimitsMode() == User) {
+        limitsMode = true;
+        limitsMax = getMaxValue();
+        limitsMin = getMinValue();
+    }
+    if(getColorMode() == Alarm_Default) strcpy(colMode, "Alarm");
+    else if(getColorMode() == Alarm_Static) strcpy(colMode, "Alarm");
+    else strcpy(colMode, "Static");
+
+    nbPV = 1;
+}
+
+
+
---
+++ caQtDM_QtControls/src/calinedemo.h
@@ -0,0 +1,189 @@
+/*
+ *  This file is part of the caQtDM Framework, developed at the Paul Scherrer Institut,
+ *  Villigen, Switzerland
+ *
+ *  The caQtDM Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The caQtDM Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the caQtDM Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2010 - 2014
+ *
+ *  Author:
+ *    Anton Mezger
+ *  Contact details:
+ *    anton.mezger@psi.ch
+ */
+
+#ifndef caLINEDEMO_H
+#define caLINEDEMO_H
+
+#include <QEvent>
+#include <qtcontrols_global.h>
+#include "fontscalingwidget.h"
+#include "caWidgetInterface.h"
+
+class QTCON_EXPORT caLineDemo : public QWidget, public FontScalingWidget, public caWidgetInterface
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString channel READ getPV WRITE setPV)
+    Q_PROPERTY(Alignment alignment READ getAlignment WRITE setAlignment)
+    Q_PROPERTY(QColor foreground READ getForeground WRITE setForeground)
+    Q_PROPERTY(QColor background READ getBackground WRITE setBackground)
+    Q_PROPERTY(colMode colorMode READ getColorMode WRITE setColorMode)
+
+    Q_PROPERTY(bool framePresent READ getFrame WRITE setFrame)
+    Q_PROPERTY(QColor frameColor READ getFrameColor WRITE setFrameColor)
+    Q_PROPERTY(int frameLineWidth READ getLineWidth WRITE setLinewidth)
+
+    Q_PROPERTY(alertHandling alarmHandling READ getAlarmHandling WRITE setAlarmHandling)
+    Q_ENUMS(alertHandling)
+
+    Q_PROPERTY(int precision READ getPrecision WRITE setPrecision)
+    Q_PROPERTY(SourceMode precisionMode READ getPrecisionMode WRITE setPrecisionMode)
+
+    Q_PROPERTY(SourceMode limitsMode READ getLimitsMode WRITE setLimitsMode)
+    Q_ENUMS(SourceMode)
+
+    Q_PROPERTY(double maxValue READ getMaxValue WRITE setMaxValue)
+    Q_PROPERTY(double minValue READ getMinValue WRITE setMinValue)
+
+    Q_PROPERTY(ScaleMode fontScaleMode READ fontScaleMode WRITE setFontScaleModeL)
+    Q_PROPERTY(bool unitsEnabled READ getUnitsEnabled WRITE setUnitsEnabled)
+    Q_PROPERTY(FormatType formatType READ getFormatType WRITE setFormatType)
+
+    Q_ENUMS(colMode)
+    Q_ENUMS(Alignment)
+    Q_ENUMS(SourceMode)
+    Q_ENUMS(ScaleMode)
+    Q_ENUMS(alertHandling)
+    Q_ENUMS(FormatType)
+
+public:
+    enum FormatType { decimal, exponential, engr_notation, compact, truncated,
+                      hexadecimal, octal, string, sexagesimal, sexagesimal_hms, sexagesimal_dms, enumeric}; // enumeric = enum as number
+    enum ScaleMode { None, Height, WidthAndHeight};
+    enum Alignment { Center, Left, Right};
+    enum alertHandling { onForeground = 0, onBackground };
+    enum colMode {Default=0, Static, Alarm_Default, Alarm_Static};
+    enum SourceMode {Channel = 0, User};
+
+    caLineDemo(QWidget *parent = 0);
+    ~caLineDemo() {}
+
+    // caWidgetInterface implementation
+    void caDataUpdate(const QString& units, const QString& String, const knobData& data);
+    void caActivate(CaQtDM_Lib_Interface* interface, QMap<QString, QString> map, knobData* kData, int* specData, QWidget* parent);
+    void getWidgetInfo(QString* pv, int& nbPV, int& limitsDefault, int& precMode, int& limitsMode,
+                                    int& Precision, char* colMode, double& limitsMax, double& limitsMin);
+    void createContextMenu(QMenu& menu);
+    QString getDragText(){ return "hello"; }
+    // caWidgetInterface implementation finish
+
+    // other stuff
+    alertHandling getAlarmHandling() const { return m_AlarmHandling;}
+    void setAlarmHandling(alertHandling alarmHandling) {m_AlarmHandling = alarmHandling;}
+
+    bool getUnitsEnabled() const { return m_UnitMode;}
+    void setUnitsEnabled(bool mode) {m_UnitMode = mode;}
+
+    double getMaxValue()  const {return m_Maximum;}
+    void setMaxValue(double const &maxim) {m_Maximum = maxim;}
+
+    double getMinValue()  const {return m_Minimum;}
+    void setMinValue(double const &minim) {m_Minimum = minim;}
+
+    QString getPV()  const {return thisPV;}
+    void setPV(QString const &newPV) {thisPV = newPV;}
+
+    void setAlignment(const Alignment &alignment);
+    Alignment getAlignment() const {return m_Alignment;}
+
+    void setFontScaleModeL(ScaleMode m) { FontScalingWidget::setScaleMode((int) m); }
+    ScaleMode fontScaleMode() { return (ScaleMode) FontScalingWidget::scaleMode();  }
+
+    void setFormatType(FormatType m) { m_FormatType = m; }
+    FormatType getFormatType() { return m_FormatType; }
+
+    bool getFrame() const { return m_FramePresent;}
+    void setFrame(bool frame);
+    QColor getFrameColor() const {return m_FrameColor;}
+    void setFrameColor(QColor c);
+    int getLineWidth() const {return m_FrameLineWidth;}
+    void setLinewidth(int width);
+
+    QColor getForeground() const {return m_ForeColor;}
+    void setForeground(QColor c);
+
+    QColor getBackground() const {return m_BackColor;}
+    void setBackground(QColor c);
+
+    colMode getColorMode() const { return m_ColorMode; }
+    void setColorMode(colMode colormode) {m_ColorMode = colormode;
+                                          setBackground(m_BackColor);
+                                          setForeground(m_ForeColor);}
+
+    SourceMode getPrecisionMode() const { return m_PrecMode; }
+    void setPrecisionMode(SourceMode precmode) {m_PrecMode = precmode;}
+
+    SourceMode getLimitsMode() const { return m_LimitsMode; }
+    void setLimitsMode(SourceMode limitsmode) { m_LimitsMode = limitsmode;}
+
+    int getPrecision() const {return m_Precision;}
+    void setPrecision(int prec) {m_Precision = prec;}
+
+    void setColors(QColor bg, QColor fg, QColor frame);
+    void setAlarmColors(short status, double value, QColor bgAtInit, QColor fgAtInit);
+
+    void forceForeAndBackground(QColor fg, QColor bg);
+    void setForeAndBackground(QColor foreground, QColor background);
+    void setText(const QString &txt);
+    QString text() const { return m_Text; }
+    void setFormat(int prec);
+    void setValue(double value, const QString& units);
+
+protected:
+    virtual bool event(QEvent *);
+    virtual QSize sizeHint() const;
+    virtual QSize minimumSizeHint() const;
+    QSize calculateTextSpace();
+    void paintEvent(QPaintEvent *);
+    CaQtDM_Lib_Interface* caDataInterface;
+
+private:
+    QString thisPV, thisText;
+    float rotation;
+    QColor m_ForeColor, m_ForeColorOld;
+    QColor m_BackColor, m_BackColorOld;
+    QColor m_BackColorDefault, m_ForeColorDefault;
+    colMode m_ColorMode, m_ColorModeOld;
+    Alignment m_Alignment;
+    alertHandling m_AlarmHandling;
+    bool m_UnitMode;
+    double m_Maximum, m_Minimum;
+    int m_Precision;
+    SourceMode m_PrecMode;
+    SourceMode m_LimitsMode;
+    FormatType m_FormatType;
+    QString m_Text;
+    char m_Format[20];
+    char m_FormatC[20];
+    short m_AlarmState;
+    QColor m_bgAtInitLast;
+    QColor m_fgAtInitLast;
+    bool m_IsShown;
+    bool m_FramePresent;
+    QColor m_FrameColor, m_FrameColorOld;
+    int m_FrameLineWidth, m_FrameLineWidthOld;
+    QColor m_FrameColorTop, m_FrameColorBottom;
+};
+#endif
