553d0e78be62c0702884dfa09837d3b58f2477a7
AntonMezger <anton.mezger@psi.ch>
2017-10-18 15:27:19 +0200

    access security ioc start script

--- caQtDM_Tests/st.cmd
+++ caQtDM_Tests/st.cmd
@@ -1,24 +1,5 @@
-#!../../bin/win32-x86/softioc
-
-## You may have to change softioc to something else
-## everywhere it appears in this file
-
-< envPaths
-
-#cd ${TOP}
-
-## Register all support components
-dbLoadDatabase "../../dbd/ioc.dbd"
-ioc_registerRecordDeviceDriver pdbbase
-
+#ld < /home/mezger/base-3.14.8.2/src/softIoc/O.linux-x86/devGaussWaveform.o
 ## Load record instances
-dbLoadRecords("X:/qt/caqtdm_project/caQtDM_Tests/mySimulation.db","user=brandsHost")
-
-
-
-
-#cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=brandsHost"
+require "sysmon"
+dbLoadTemplate("SF-SGUIDE_main.subs")
+iocInit()
