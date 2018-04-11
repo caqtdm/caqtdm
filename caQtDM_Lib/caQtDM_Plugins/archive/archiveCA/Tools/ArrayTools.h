00ff32da394498b61175dd8389975588eefa5249
AntonMezger <anton.mezger@psi.ch>
2018-04-11 16:10:49 +0200

    files removed while now in caQtDM_Archiver git

--- caQtDM_Lib/caQtDM_Plugins/archive/archiveCA/Tools/ArrayTools.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ARRAYTOOLS_H__
-#define __ARRAYTOOLS_H__
-
-#ifndef SIZEOF_ARRAY
-#define SIZEOF_ARRAY(ar)        (sizeof(ar)/sizeof((ar)[0]))
-#endif // !defined(SIZEOF_ARRAY)
-
-#endif //__ARRAYTOOLS_H__
