243f9ebe7a94656b2bf418cfcb48156e1b192e1a
AntonMezger <anton.mezger@psi.ch>
2018-05-02 06:19:06 -0400

    newer ioc shell file

--- caQtDM_Tests/st.cmd
+++ caQtDM_Tests/st.cmd
@@ -1,6 +1,4 @@
-#ld < /home/mezger/base-3.14.8.2/src/softIoc/O.linux-x86/devGaussWaveform.o
+require array
 ## Load record instances
 dbLoadDatabase ("mySimulation.db", "", "")
-#dbLoadDatabase ("as.tpl", "", "")
-#asSetFilename("as.as")
 iocInit()
