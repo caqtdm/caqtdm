184c5a6abe2c68dbd7b08eb3a82c9381234d0e28
Helge Brands <helge.brands@psi.ch>
2022-02-22 10:39:52 +0100

    remove designable function call

--- caQtDM_QtControls/src/cacartesianplot.h
+++ caQtDM_QtControls/src/cacartesianplot.h
@@ -80,42 +80,42 @@ class QTCON_EXPORT caCartesianPlot : public QwtPlot
     Q_PROPERTY(QString TitleY READ getTitleY WRITE setTitleY)
 
     Q_PROPERTY(QStringList channelList_1 READ getPV1List WRITE setPV1List STORED false)
-    Q_PROPERTY(QString channels_1 READ getPV_1 WRITE setPV_1 DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_1 READ getPV_1 WRITE setPV_1 DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_1 READ getStyle_1 WRITE setStyle_1)
     Q_PROPERTY(curvSymbol symbol_1 READ getSymbol_1 WRITE setSymbol_1)
     Q_PROPERTY(QColor color_1 READ getColor_1 WRITE setColor_1)
 
     Q_PROPERTY(QStringList channelList_2 READ getPV2List WRITE setPV2List STORED false)
-    Q_PROPERTY(QString channels_2 READ getPV_2 WRITE setPV_2  DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_2 READ getPV_2 WRITE setPV_2  DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_2 READ getStyle_2 WRITE setStyle_2)
     Q_PROPERTY(curvSymbol symbol_2 READ getSymbol_2 WRITE setSymbol_2)
     Q_PROPERTY(QColor color_2 READ getColor_2 WRITE setColor_2)
 
     Q_PROPERTY(QStringList channelList_3 READ getPV3List WRITE setPV3List STORED false)
-    Q_PROPERTY(QString channels_3 READ getPV_3 WRITE setPV_3 DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_3 READ getPV_3 WRITE setPV_3 DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_3 READ getStyle_3 WRITE setStyle_3)
     Q_PROPERTY(curvSymbol symbol_3 READ getSymbol_3 WRITE setSymbol_3)
     Q_PROPERTY(QColor color_3 READ getColor_3 WRITE setColor_3)
 
     Q_PROPERTY(QStringList channelList_4 READ getPV4List WRITE setPV4List STORED false)
-    Q_PROPERTY(QString channels_4 READ getPV_4 WRITE setPV_4  DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_4 READ getPV_4 WRITE setPV_4  DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_4 READ getStyle_4 WRITE setStyle_4)
     Q_PROPERTY(curvSymbol symbol_4 READ getSymbol_4 WRITE setSymbol_4)
     Q_PROPERTY(QColor color_4 READ getColor_4 WRITE setColor_4)
 
     Q_PROPERTY(QStringList channelList_5 READ getPV5List WRITE setPV5List STORED false)
-    Q_PROPERTY(QString channels_5 READ getPV_5 WRITE setPV_5  DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_5 READ getPV_5 WRITE setPV_5  DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_5 READ getStyle_5 WRITE setStyle_5)
     Q_PROPERTY(curvSymbol symbol_5 READ getSymbol_5 WRITE setSymbol_5)
     Q_PROPERTY(QColor color_5 READ getColor_5 WRITE setColor_5)
 
     Q_PROPERTY(QStringList channelList_6 READ getPV6List WRITE setPV6List STORED false)
-    Q_PROPERTY(QString channels_6 READ getPV_6 WRITE setPV_6  DESIGNABLE inactiveButVisible())
+    Q_PROPERTY(QString channels_6 READ getPV_6 WRITE setPV_6  DESIGNABLE false)
 
     Q_PROPERTY(curvStyle Style_6 READ getStyle_6 WRITE setStyle_6)
     Q_PROPERTY(curvSymbol symbol_6 READ getSymbol_6 WRITE setSymbol_6)
