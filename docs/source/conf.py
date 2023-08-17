fc809bbc244af26b7ef8a458a2c341a3dcea46be
Pete R Jemian <prjemian@gmail.com>
2023-08-17 16:04:59 -0500

    MNT #27 change theme, typos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'caQtDM'
-copyright = '2023, Anton Metzger, Helge Brands'
-author = 'Anton Metzger, Helge Brands'
-release = '4.4.0'
+project = "caQtDM"
+copyright = "2007, Anton Mezger, Helge Brands"  # GPL3
+author = "Anton Mezger, Helge Brands"
+release = "4.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = """
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.githubpages
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+""".split()
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
+source_suffix = ".rst .md".split()
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_theme = 'sphinxdoc'
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
